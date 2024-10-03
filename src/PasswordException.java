6b532ddfa1195e009bf222697f0d7c6aec8ab038
arigonzal <151198787+arigonzal@users.noreply.github.com>
2024-10-02 20:57:32 -0700

    Update PasswordException.java
    DONE

--- src/PasswordException.java
+++ src/PasswordException.java
@@ -1,35 +1,35 @@
-public abstract class PasswordException extends Exception {
-  public PasswordException (String msg) {
-    super(msg);
-  }
+class PasswordException extends Exception {
+    public PasswordException(String message) {
+        super(message);
+    }
 }
 
 class UpperCaseCharacterMissing extends PasswordException {
-  public UpperCaseCharacterMissing() {
-    super("Password must contain at least one uppercase letter.");
-  }
+    public UpperCaseCharacterMissing() {
+        super("Password must contain at least one uppercase letter.");
+    }
 }
 
 class LowerCaseCharacterMissing extends PasswordException {
-  public LowerCaseCharacterMissing() {
-    super("Password must contain at least one lowercase letter.");
-  }
+    public LowerCaseCharacterMissing() {
+        super("Password must contain at least one lowercase letter.");
+    }
 }
 
 class SpecialCharacterMissing extends PasswordException {
-  public SpecialCharacterMissing() {
-    super("Password must contain at least one special character like !@#$%^&*()");
-  }
+    public SpecialCharacterMissing() {
+        super("Password must contain at least one special character like !@#$%^&*()");
+    }
 }
 
 class NumberCharacterMissing extends PasswordException {
-  public NumberCharacterMissing() {
-    super("Password must contain at least one number.");
-  }
+    public NumberCharacterMissing() {
+        super("Password must contain at least one number.");
+    }
 }
 
 class Minimum8CharactersRequired extends PasswordException {
-  public Minimum8CharactersRequired() {
-    super("Password must be at least 8 characters long.");
-  }
+    public Minimum8CharactersRequired() {
+        super("Password must be at least 8 characters long.");
+    }
 }
